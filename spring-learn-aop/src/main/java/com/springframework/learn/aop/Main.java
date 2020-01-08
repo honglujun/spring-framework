251a821c3c5069c5f0c6bfa2139920f3803be4e2
honglujun <909408954@qq.com>
2020-01-08 16:34:56 +0800

    修改了spring的版本，重新修改了gradle的配置文件，通过spring-learn-aop进行了测试

--- spring-learn-aop/src/main/java/com/springframework/learn/aop/Main.java
+++ spring-learn-aop/src/main/java/com/springframework/learn/aop/Main.java
@@ -1,14 +1,19 @@
 package com.springframework.learn.aop;
 
-import com.springframework.learn.aop.app.Appconfig;
+import com.springframework.learn.aop.app.AppConfig;
 import com.springframework.learn.aop.dao.TestDao;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Appconfig.class);
+		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		TestDao testDao = annotationConfigApplicationContext.getBean(TestDao.class);
+		AppConfig bean = annotationConfigApplicationContext.getBean(AppConfig.class);
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext();
+
 		testDao.query();
+		bean.outPrint();
 	}
 }
--- spring-learn-aop/src/main/java/com/springframework/learn/aop/app/AppConfig.java
+++ spring-learn-aop/src/main/java/com/springframework/learn/aop/app/AppConfig.java
@@ -3,7 +3,14 @@ package com.springframework.learn.aop.app;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 这个类相当于xml的bean文件
+ */
 @Configuration
 @ComponentScan("com.springframework")
-public class Appconfig {
+public class AppConfig {
+
+	public void outPrint() {
+		System.out.println("打印AppConfig的outPrint()");
+	}
 }
